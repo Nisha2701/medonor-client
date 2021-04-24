e03d04451e72eeaeac98c8b68834b152a3b7ebd0
20adi01 <aditipanigrahi2001@gmail.com>
2021-04-24 22:16:58 +0530

    donor_speak

---
+++ src/components/donor_speaks/DonorSpeaks.js
@@ -0,0 +1,72 @@
+import React from "react";
+import "./DonorSpeaks.css";
+
+function DonorSpeaks() {
+    
+    return (
+        <div className="container">
+            <div className="title">
+                <h2 data-text="Donor Speaks">Donor Speaks</h2>
+            </div>
+            <hr className="sep-2" />
+            <div className="row">
+                <div className="column">
+                    <div className="card">
+                        <p>
+      	                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate ab tenetur 		at delectus aspernatur similique modi, est, debitis sit voluptate dolorem 			eligendi odio laborum magni nesciunt? Aliquam omnis molestiae at.
+                        </p>
+                        <p className="donorName">-Name</p>
+                    </div>
+                </div>
+
+                <div className="column">
+                    <div className="card">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate ab tenetur 		at delectus aspernatur similique modi, est, debitis sit voluptate dolorem 			eligendi odio laborum magni nesciunt? Aliquam omnis molestiae at.
+                        </p>
+                        <p className="donorName">-Name</p>
+                    </div>
+                </div>
+  
+                <div className="column">
+                    <div className="card">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate ab tenetur 		at delectus aspernatur similique modi, est, debitis sit voluptate dolorem 			eligendi odio laborum magni nesciunt? Aliquam omnis molestiae at.
+                        </p>
+                        <p className="donorName">-Name</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="row">
+                <div className="column">
+                    <div className="card">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate ab tenetur 		at delectus aspernatur similique modi, est, debitis sit voluptate dolorem 			eligendi odio laborum magni nesciunt? Aliquam omnis molestiae at.
+                        </p>
+                        <p className="donorName">-Name</p>
+                    </div>
+                </div>
+
+                <div className="column">
+                    <div className="card">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate ab tenetur 		at delectus aspernatur similique modi, est, debitis sit voluptate dolorem 			eligendi odio laborum magni nesciunt? Aliquam omnis molestiae at.
+                        </p>
+                        <p className="donorName">-Name</p>
+                    </div>
+                </div>
+  
+                <div className="column">
+                    <div className="card">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate ab tenetur 		at delectus aspernatur similique modi, est, debitis sit voluptate dolorem 			eligendi odio laborum magni nesciunt? Aliquam omnis molestiae at.
+                        </p>
+                        <p className="donorName">-Name</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+  }
+  export default DonorSpeaks;
