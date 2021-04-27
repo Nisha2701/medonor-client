040b65cc8280be908d2a65ffb17866c2ac1b4ea3
Dishebh Bhayana <dishebh27@gmail.com>
2021-04-27 21:09:46 +0530

    userReducer fix

--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -1,60 +1,63 @@
-import * as actionTypes from "../ActionTypes";
-import axios from "../axios";
+import * as actionTypes from '../ActionTypes';
+import axios from '../axios';
 //npm i axios
 
-export const LoginInit = (loginCred)=>{
-    return (dispatch)=>{
-        console.log(loginCred)
-        const requestURL = "users/login";
-        axios.post(requestURL, loginCred)
-        .then((res)=>{
-            console.log(res);
-            localStorage.setItem("token",res.data.token)
-            dispatch({type:actionTypes.LOGIN_SUCCESS,token:res.data.token})
-        })
-        .catch((err)=>{
-            console.log(err)
-            dispatch({type:actionTypes.LOGOUT_FAILED})
-        })
-    }
-}
+export const LoginInit = (loginCred) => {
+  return (dispatch) => {
+    console.log(loginCred);
+    const requestURL = 'users/login';
+    axios
+      .post(requestURL, loginCred)
+      .then((res) => {
+        localStorage.setItem('token', res.data.token);
+        dispatch({
+          type: actionTypes.LOGIN_SUCCESS,
+          token: res.data.token,
+          payload: res.data.user,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        dispatch({ type: actionTypes.LOGOUT_FAILED });
+      });
+  };
+};
 
-export const signupInit = (signupCred)=>{
-    return (dispatch)=>{
-        console.log(signupCred)
-        const requestURL = "users/signup";
-        axios.post(requestURL, signupCred)
-        .then((res)=>{
-          console.log(res);
-        })
-        .catch((err)=>{
-            console.log(err)
-        })
-    }
-}
+export const signupInit = (signupCred) => {
+  return (dispatch) => {
+    console.log(signupCred);
+    const requestURL = 'users/signup';
+    axios
+      .post(requestURL, signupCred)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+};
 
-
-export const logout = ()=>{
-    return (dispatch)=>{
-        const requestURL = "/";
-        axios.get(requestURL)
-        .then((res)=>{
-            console.log(res)
-            localStorage.removeItem("Username")
-            localStorage.removeItem("email")
-            localStorage.removeItem("role")
-            localStorage.removeItem("token")
-            dispatch({
-                type : actionTypes.LOGOUT_SUCCESS
-            })
-
-        })
-        .catch((err)=>{
-            console.log(err)
-            dispatch({
-                type : actionTypes.LOGOUT_FAILED
-            })
-        })      
-    }
-}
-
+export const logout = () => {
+  return (dispatch) => {
+    const requestURL = '/';
+    axios
+      .get(requestURL)
+      .then((res) => {
+        console.log(res);
+        localStorage.removeItem('Username');
+        localStorage.removeItem('email');
+        localStorage.removeItem('role');
+        localStorage.removeItem('token');
+        dispatch({
+          type: actionTypes.LOGOUT_SUCCESS,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        dispatch({
+          type: actionTypes.LOGOUT_FAILED,
+        });
+      });
+  };
+};
--- src/redux/reducers/userReducer.js
+++ src/redux/reducers/userReducer.js
@@ -1,25 +1,33 @@
-import * as actionTypes from "../ActionTypes";
+import * as actionTypes from '../ActionTypes';
 
 const initialState = {
-    Username : null || localStorage.getItem("Username"),
-    email : null || localStorage.getItem("email"),
-    role : null || localStorage.getItem("role"),
-    token : null || localStorage.getItem("token")
-}
+  Username: null || localStorage.getItem('Username'),
+  email: null || localStorage.getItem('email'),
+  role: null || localStorage.getItem('role'),
+  token: null || localStorage.getItem('token'),
+};
 
-const UserReducer = (state= initialState, action)=>{
-    switch(action.type){
-
-        case actionTypes.LOGOUT_SUCCESS :
-            return {
-                ...state,
-                Username : null,
-                email : null,
-                role : null,
-                token : null
-            }
-        default : return state;
-    }
-}
+const UserReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.LOGOUT_SUCCESS:
+      return {
+        ...state,
+        Username: null,
+        email: null,
+        role: null,
+        token: null,
+      };
+    case actionTypes.LOGIN_SUCCESS:
+      return {
+        ...state,
+        Username: action.payload.username,
+        // email: action.payload.email,
+        role: action.payload.role,
+        token: action.token,
+      };
+    default:
+      return state;
+  }
+};
 
 export default UserReducer;
