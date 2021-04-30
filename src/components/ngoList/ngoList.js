b6b763ef0aa6355fcf44588a4e75265f54bbb421
jatingupta9939 <guptajatin9939@gmail.com>
2021-04-30 17:16:31 +0530

    ngolisttable

--- src/components/ngoList/ngoList.js
+++ src/components/ngoList/ngoList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
-import "./ngoList.css";
+import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import Table from 'react-bootstrap/Table';
+import "./ngoList.css";
 
 class ngoList extends Component {
     constructor(props) {
@@ -14,45 +15,104 @@ class ngoList extends Component {
           </div>
           <br></br>
           <div className="body-text table-setting">
-        <Table hover >
+        <Table hover className="tablepad" >
         <thead>
-          <tr>
+          <tr className="tblrow">
             
-            <th className="text-center table-heading">NGO</th>
-            <th className="text-center table-heading">Address</th>
-            <th className="text-center table-heading" >Contact</th>
+            <th className="text-center table-heading theader">NGO</th>
+            <th className="text-center table-heading theader">Address</th>
+            <th className="text-center table-heading theader" >Contact</th>
             
           </tr>
           
         </thead>
         <tbody>
-          <tr>
             
-            <td>Ngo name</td>
-            
-            <td className="text-center">Sector - 7</td>
-            <td className="text-center last-col">
-            <p>9836462788<br/>ngo@gmail.com </p>
-            </td>
-            
-          </tr>
-          <tr>
-            
-            <td>Ngo name</td>
-            <td className="text-center">Sector - 40</td>
-            <td className="text-center last-col">
-                <p>9836462788<br/>ngo@gmail.com </p>
-            
+              <tr className="tblrow">
+                
+                <td>HelpAge India</td>
+                
+                <td className="text-center">House No. 5745 GF, Market Rd, Sector 38 West, Chandigarh</td>
+                <td className="text-center last-col">
+                <p> 0172 269 3869<br/> www.helpageindia.org  </p>
                 </td>
-          </tr>
-          <tr>
-            
-            <td>Ngo name</td>
-            <td className="text-center">Sector - 40</td>
-            <td className="text-center last-col">
-            <p>9836462788<br/>ngo@gmail.com </p>
-            </td>
-          </tr>
+                
+              </tr>
+
+              
+              <tr className="tblrow">
+                
+                <td>Society for the Prevention of Cruelty to Animals</td>
+                <td className="text-center">Sector 38 West, Near Motor Market, Opposite Chandigarh Transport Depot, Chandigarh</td>
+                <td className="text-center last-col">
+                    <p>0172 269 6450<br/>www.spcachandigarh.org  </p>
+                
+                    </td>
+              </tr>
+              <tr className="tblrow">
+                
+                <td>Nanhe Kadam</td>
+                <td className="text-center">S.C.O.36 (First Floor) Sector 26, Madhya Marg, Chandigarh</td>
+                <td className="text-center last-col">
+                <p>NA<br/> www.nanhekadam.org</p>
+                </td>
+              </tr>
+              <tr className="tblrow">
+                
+                <td>Saarthi</td>
+                <td className="text-center">573,  Near Govt. Middle School, Kishangarh, Chandigarh</td>
+                <td className="text-center last-col">
+                <p>094639 71544<br/>www.saarthi.org.in  </p>
+                </td>
+              </tr>
+              <tr className="tblrow">
+                
+                <td>ArriveSAFE</td>
+                <td className="text-center">H No. 268, Sector 21A, Chandigarh</td>
+                <td className="text-center last-col">
+                <p>0172 462 3232<br/> www.arrivesafe.org </p>
+                </td>
+              </tr>
+              <tr className="tblrow">
+                
+                <td>Sankalp NGO</td>
+                <td className="text-center">3156, Sector 38 B Rd, Sector 38-D, Sector 38, Chandigarh</td>
+                <td className="text-center last-col">
+                <p>082838 63156<br/>www.sankalpngo.org.in</p>
+                </td>
+              </tr>
+              <tr className="tblrow">
+                
+                <td>Salaam Zindagi Charitable Trust</td>
+                <td className="text-center">#2854, Sector 38-C, Chandigarh</td>
+                <td className="text-center last-col">
+                <p>1800 137 1317<br/>www.salaamzindagi.org</p>
+                </td>
+              </tr>
+              <tr className="tblrow">
+                
+                <td>EduCARE India</td>
+                <td className="text-center">New Colony, Khudda Lahora, Chandigarh</td>
+                <td className="text-center last-col">
+                <p>098158 61995<br/>www.educare.in </p>
+                </td>
+              </tr>
+              <tr className="tblrow">
+                
+                <td>Safe Hands Rehabilitation Society</td>
+                <td className="text-center">564/04, Shivalik Vihar, Chandigarh</td>
+                <td className="text-center last-col">
+                <p>084270 09040<br/>www.shrs.ngo</p>
+                </td>
+              </tr>
+              <tr className="tblrow">
+                
+                <td>Nav Chetna</td>
+                <td className="text-center"> Office No. 22 Near Shop No. 22, Near Guga Mari Temple, Khuda Lahora, Chandigarh</td>
+                <td className="text-center last-col">
+                <p>098887 54488<br/>www.navchetna.ngo </p>
+                </td>
+              </tr>
           
         </tbody>
       </Table>
