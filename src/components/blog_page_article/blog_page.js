79ca2838352071b113ef0d6c02dc2b779f990cb5
Dishebh Bhayana <dishebh27@gmail.com>
2021-05-06 00:06:03 +0530

    fixes

--- src/components/blog_page_article/blog_page.js
+++ src/components/blog_page_article/blog_page.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './blog_page.css';
 import img1 from './pic.png';
@@ -16,6 +16,7 @@ import {
   Card,
   Jumbotron,
 } from 'react-bootstrap';
+import { useHistory } from 'react-router';
 
 const mapStateToProps = (state) => {
   return {
@@ -29,11 +30,14 @@ const mapDispatchToProps = (dispatch) => {
   };
 };
 
-class blog extends Component {
-  componentDidMount() {
-    this.props.fetchNgoBlogs();
-  }
-  renderNgoBlogs = (ngoBlogs, key) => {
+const Blog = ({ fetchNgoBlogs, ngoBlogs }) => {
+  const history = useHistory();
+
+  useEffect(() => {
+    fetchNgoBlogs();
+  }, [fetchNgoBlogs]);
+
+  const renderNgoBlogs = (ngoBlogs, key) => {
     return (
       <Row className='article-card' key={key}>
         <div>
@@ -50,7 +54,14 @@ class blog extends Component {
             <Card.Text className='card-title-desc'>
               {ngoBlogs.author.name} | 23 Apr 2021
             </Card.Text>
-            <Button variant='light' size='sm' className='read-more-btn'>
+            <Button
+              onClick={() => {
+                history.push(`/${ngoBlogs._id}`);
+              }}
+              variant='light'
+              size='sm'
+              className='read-more-btn'
+            >
               Read more
             </Button>
           </Card.Body>
@@ -58,138 +69,132 @@ class blog extends Component {
       </Row>
     );
   };
-  render() {
-    console.log('isLoading!', this.props.ngoBlogs);
-    if (this.props.ngoBlogs.isLoading) {
-      return <h1>Loading</h1>;
-    } else if (this.props.ngoBlogs.errMess) {
-      return <h1>{this.props.ngoBlogs.errMess}</h1>;
-    }
-    return (
-      <div className='blogpage'>
-        <Jumbotron
-          fluid
-          className='blog-title-main'
-          style={{ backgroundColor: '#fff' }}
-        >
-          <Container>
-            <p style={{ fontSize: '6rem' }} className='blog-title edit-1'>
-              MEDBLOG{' '}
-              <img
-                src='https://www.cry.org/wp-content/themes/cry/images/streak.gif'
-                className='streak-img'
-              />
-            </p>
-            <p style={{ fontSize: '3.5rem' }} className='blog-title-byline'>
-              Amazing people. Amazing stories
-            </p>
-            <p style={{ fontSize: '2rem' }} className='blog-title-byline-by'>
-              If you’re looking for positive stories of change that will make
-              your day, you’ve come to the right place!{' '}
-            </p>
-          </Container>
-        </Jumbotron>
 
-        <Container className='blog-posts'>
-          <Row className='row-of-article '>
-            <Col className='left-col'>
-              <h2 className='hr-line'>
-                <span className='hr-line-content'>LATEST POSTS</span>
-              </h2>
-              <div classname='row'>
-                {this.props.ngoBlogs.ngoblogs.map((item, key) => 
-                  this.renderNgoBlogs(item, key),
-                )}
-              </div>
+  console.log('isLoading!', ngoBlogs);
+  if (ngoBlogs.isLoading) {
+    return <h1>Loading</h1>;
+  } else if (ngoBlogs.errMess) {
+    return <h1>{ngoBlogs.errMess}</h1>;
+  }
+
+  return (
+    <div className='blogpage'>
+      <Jumbotron
+        fluid
+        className='blog-title-main'
+        style={{ backgroundColor: '#fff' }}
+      >
+        <Container>
+          <p style={{ fontSize: '6rem' }} className='blog-title edit-1'>
+            MEDBLOG{' '}
+            <img
+              src='https://www.cry.org/wp-content/themes/cry/images/streak.gif'
+              className='streak-img'
+            />
+          </p>
+          <p style={{ fontSize: '3.5rem' }} className='blog-title-byline'>
+            Amazing people. Amazing stories
+          </p>
+          <p style={{ fontSize: '2rem' }} className='blog-title-byline-by'>
+            If you’re looking for positive stories of change that will make your
+            day, you’ve come to the right place!{' '}
+          </p>
+        </Container>
+      </Jumbotron>
+
+      <Container className='blog-posts'>
+        <Row className='row-of-article '>
+          <Col className='left-col'>
+            <h2 className='hr-line'>
+              <span className='hr-line-content'>LATEST POSTS</span>
+            </h2>
+            <div classname='row'>
+              {ngoBlogs.ngoblogs.map((item, key) => renderNgoBlogs(item, key))}
+            </div>
+          </Col>
+
+          <Col xs lg='4' className='right-col'>
+            <Card.Body className='right-col-card'>
+              <Card.Title
+                className=' card-title-right'
+                style={{ fontSize: '1.7rem' }}
+              >
+                About Us
+              </Card.Title>
+              <hr className='hr-right' />
+              <Card.Text className='card-title-desc-right'>
+                With your support, we address people's critical needs of
+                medication by working with NGOs and various communities,
+                district and state level governments as well as the patients
+                themselves. We focus on supplying medication and influencing
+                public to stop medicine destruction at all levels.
+              </Card.Text>
+            </Card.Body>
+          </Col>
+        </Row>
+      </Container>
+
+      <Jumbotron className='note-jumbo' style={{ backgroundColor: '#DAF1F0' }}>
+        <Container>
+          <Row>
+            <Col>
+              <Card body border='light' className='note-card'>
+                <Row className='small-note-content'>
+                  <Col classname='note-sub-col'>
+                    <p>
+                      <img src={pic1} className='small-note-image' />
+                    </p>
+                  </Col>
+                  <Col md='auto'>
+                    <p className='small-note' style={{ fontSize: '1.3rem' }}>
+                      {' '}
+                      All our efforts are made possible only because of your
+                      support
+                    </p>
+                  </Col>
+                </Row>
+              </Card>
             </Col>
-
-            <Col xs lg='4' className='right-col'>
-              <Card.Body className='right-col-card'>
-                <Card.Title
-                  className=' card-title-right'
-                  style={{ fontSize: '1.7rem' }}
-                >
-                  About Us
-                </Card.Title>
-                <hr className='hr-right' />
-                <Card.Text className='card-title-desc-right'>
-                  With your support, we address people's critical needs of
-                  medication by working with NGOs and various communities,
-                  district and state level governments as well as the patients
-                  themselves. We focus on supplying medication and influencing
-                  public to stop medicine destruction at all levels.
-                </Card.Text>
-              </Card.Body>
+            <Col>
+              <Card body border='light' className='note-card'>
+                <Row className='small-note-content'>
+                  <Col classname='note-sub-col'>
+                    <p>
+                      <img src={pic2} className='small-note-image' />
+                    </p>
+                  </Col>
+                  <Col md='auto'>
+                    <p className='small-note' style={{ fontSize: '1.3rem' }}>
+                      {' '}
+                      Your donations are tax exempted under 80G of the Indian
+                      Income Tax Act
+                    </p>
+                  </Col>
+                </Row>
+              </Card>
+            </Col>
+            <Col>
+              <Card body border='light' className='note-card'>
+                <Row className='small-note-content'>
+                  <Col classname='note-sub-col'>
+                    <p>
+                      <img src={pic3} className='small-note-image' />
+                    </p>
+                  </Col>
+                  <Col md='auto'>
+                    <p className='small-note' style={{ fontSize: '1.3rem' }}>
+                      {' '}
+                      Your donation transactions are completely safe and secure
+                    </p>
+                  </Col>
+                </Row>
+              </Card>
             </Col>
           </Row>
         </Container>
+      </Jumbotron>
+    </div>
+  );
+};
 
-        <Jumbotron
-          className='note-jumbo'
-          style={{ backgroundColor: '#DAF1F0' }}
-        >
-          <Container>
-            <Row>
-              <Col>
-                <Card body border='light' className='note-card'>
-                  <Row className='small-note-content'>
-                    <Col classname='note-sub-col'>
-                      <p>
-                        <img src={pic1} className='small-note-image' />
-                      </p>
-                    </Col>
-                    <Col md='auto'>
-                      <p className='small-note' style={{ fontSize: '1.3rem' }}>
-                        {' '}
-                        All our efforts are made possible only because of your
-                        support
-                      </p>
-                    </Col>
-                  </Row>
-                </Card>
-              </Col>
-              <Col>
-                <Card body border='light' className='note-card'>
-                  <Row className='small-note-content'>
-                    <Col classname='note-sub-col'>
-                      <p>
-                        <img src={pic2} className='small-note-image' />
-                      </p>
-                    </Col>
-                    <Col md='auto'>
-                      <p className='small-note' style={{ fontSize: '1.3rem' }}>
-                        {' '}
-                        Your donations are tax exempted under 80G of the Indian
-                        Income Tax Act
-                      </p>
-                    </Col>
-                  </Row>
-                </Card>
-              </Col>
-              <Col>
-                <Card body border='light' className='note-card'>
-                  <Row className='small-note-content'>
-                    <Col classname='note-sub-col'>
-                      <p>
-                        <img src={pic3} className='small-note-image' />
-                      </p>
-                    </Col>
-                    <Col md='auto'>
-                      <p className='small-note' style={{ fontSize: '1.3rem' }}>
-                        {' '}
-                        Your donation transactions are completely safe and
-                        secure
-                      </p>
-                    </Col>
-                  </Row>
-                </Card>
-              </Col>
-            </Row>
-          </Container>
-        </Jumbotron>
-      </div>
-    );
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(blog);
+export default connect(mapStateToProps, mapDispatchToProps)(Blog);
--- src/components/medicinesList/medicinelist.js
+++ src/components/medicinesList/medicinelist.js
@@ -1,98 +1,104 @@
-import React, { Component } from 'react'
-import "./medicinelist.css";
+import React, { Component } from 'react';
+import './medicinelist.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from 'react-bootstrap/Card';
 
-import { Row, Col } from "react-bootstrap";
+import { Row, Col } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
-import { connect } from 'react-redux' ; 
-import { fetchMedicines,deleteMedicine } from '../../redux/actions/medicines';
+import { connect } from 'react-redux';
+import { fetchMedicines, deleteMedicine } from '../../redux/actions/medicines';
 
 const mapStateToProps = (state) => {
   return {
     medicines: state.medicines,
-    role:state.user.role,
-    token:state.user.token,
+    role: state.user.role,
+    token: state.user.token,
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
     fetchMedicines: () => dispatch(fetchMedicines()),
-    deleteMedicine:()=>dispatch(deleteMedicine())
-  }
-}
+    deleteMedicine: () => dispatch(deleteMedicine()),
+  };
+};
 
 class medlist extends Component {
-    
-    componentDidMount() {
-      this.props.fetchMedicines();
-    }
+  componentDidMount() {
+    this.props.fetchMedicines();
+  }
 
-    renderMedicines = (medicines,key) => {
-      return (
-        <div>
+  renderMedicines = (medicines, key) => {
+    return (
+      <div>
         <Col md={4} key={key}>
-         
-         <Card className="cdd-style bkg-im">
-  
-        <Card.Body>
-     
-      
-        <Card.Title className="medi-name">{medicines.name}</Card.Title>
-  
-       <br></br>
-      <Card.Text>
-       <p><i className="cdd-text">Amount: </i>{medicines.amount}</p>
-       <p><i className="cdd-text">Expiry: </i>{medicines.expirydate}</p>
-       <p><i className="cdd-text">Donor: </i>{medicines.author.name}</p>
-       <p><i className="cdd-text">Address: </i>{medicines.author.address}</p>
-       </Card.Text>
-       <Button variant="outline-primary"  className="btt-style">Contact now</Button>{' '}
-       {
-        this.props.role==='admin' || this.props.userId === medicines.author._id
-        ?
-    
-            <Button color='danger' onClick={() => deleteMedicine(medicines._id)}>
-            <span className='fa fa-lg fa-trash'></span></Button>
-    
-        :
-        <></>
-    }
-       </Card.Body>
-       </Card>
-       </Col>  
-        </div>
-      )
-    }
+          <Card className='cdd-style bkg-im'>
+            <Card.Body>
+              <Card.Title className='medi-name'>{medicines.name}</Card.Title>
+              <br></br>
+              <Card.Text>
+                <p>
+                  <i className='cdd-text'>Amount: </i>
+                  {medicines.amount}
+                </p>
+                <p>
+                  <i className='cdd-text'>Expiry: </i>
+                  {medicines.expirydate}
+                </p>
+                <p>
+                  <i className='cdd-text'>Donor: </i>
+                  {medicines.author.name}
+                </p>
+                <p>
+                  <i className='cdd-text'>Address: </i>
+                  {medicines.author.address}
+                </p>
+              </Card.Text>
+              <Button variant='outline-primary' className='btt-style'>
+                Contact now
+              </Button>{' '}
+              {this.props.role === 'admin' ||
+              this.props.userId === medicines.author._id ? (
+                <Button
+                  color='danger'
+                  onClick={() => deleteMedicine(medicines._id)}
+                >
+                  <span className='fa fa-lg fa-trash'></span>
+                </Button>
+              ) : (
+                <></>
+              )}
+            </Card.Body>
+          </Card>
+        </Col>
+      </div>
+    );
+  };
 
-    render() {
-      if(this.props.medicines.isLoading) {
-        return <h1>Loading</h1>
-      } else if(this.props.medicines.errMess) {
-        return <h1>{this.props.medicines.errMess}</h1>
-      }
-      return (
-        
-        <div >
-          <div className="head text-center">
+  render() {
+    if (this.props.medicines.isLoading) {
+      return <h1>Loading</h1>;
+    } else if (this.props.medicines.errMess) {
+      return <h1>{this.props.medicines.errMess}</h1>;
+    }
+    return (
+      <div>
+        <div className='head text-center'>
           <h1>Medicines Available</h1>
-          </div>
-         
-          <hr></hr>
-          <br></br>
+        </div>
 
-          <div className="containerr">
-        <Row>
-          {this.props.medicines.medicines.map((item,key) =>
-            this.renderMedicines(item,key)
-             
+        <hr></hr>
+        <br></br>
+
+        <div className='containerr'>
+          <Row>
+            {this.props.medicines.medicines.map((item, key) =>
+              this.renderMedicines(item, key)
             )}
-      </Row>
-      </div> 
-  </div>
-
-)
+          </Row>
+        </div>
+      </div>
+    );
+  }
 }
-}
-export default connect(mapStateToProps,mapDispatchToProps)(medlist);
+export default connect(mapStateToProps, mapDispatchToProps)(medlist);
